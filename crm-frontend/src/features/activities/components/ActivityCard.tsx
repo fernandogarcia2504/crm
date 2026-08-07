bfeb88f15f473a3728da40c792b486c8b6c84738
fernando2504 <nando.garcia2504@gmail.com>
2026-08-06 23:13:52 -0600

    activity page done

---
+++ crm-frontend/src/features/activities/components/ActivityCard.tsx
@@ -0,0 +1,69 @@
+import { Phone } from "lucide-react";
+import { StickyNote } from "lucide-react";
+import { Calendar } from "lucide-react";
+import { Mail } from "lucide-react";
+
+type ActivityType = "call" | "email" | "meeting" | "note";
+
+interface SetActivityCardProps {
+    subject: string;
+    result: string;
+    nextStep: string;
+    date: string;
+    type: ActivityType;
+    isLast?: boolean;
+}
+
+const activityConfig = {
+    call: {
+        icon: Phone,
+        color: "#2F76D2"
+    },
+    meeting: {
+        icon: Calendar,
+        color: "#6B2FD2"
+    },
+    note: {
+        icon: StickyNote,
+        color: "#D2CC2F"
+    },
+    email: {
+        icon: Mail,
+        color: "#2FD260"
+    }
+}
+
+export default function({ subject, result, nextStep, date, type, isLast=false}: SetActivityCardProps) {
+
+    const {icon: Icon, color} = activityConfig[type];
+
+    return(
+        <div className="w-full grid grid-cols-[3%_60%_25%_10%] px-3 items-center gap-2">
+
+            <div className="flex flex-col items-center relative">
+                <div
+                    className="w-9 h-9 rounded-full flex items-center justify-center z-10"
+                    style={{ backgroundColor: color }}
+                >
+                    <Icon size={18} />
+                </div>
+
+                {!isLast && (
+                    <div
+                        className="absolute top-9 left-1/2 -translate-x-1/2 w-0 h-10 border-l-2 border-dashed border-gray-500"
+                    />
+                )}
+
+            </div>
+
+            <div className="flex flex-col ">
+                <p>{subject}</p>
+                <p className="text-sm text-[#959595]">{nextStep}</p>
+            </div>
+
+            <p className="text-sm text-[#959595]">{date}</p>
+
+            <p className="text-sm text-[#959595] text-right">{result}</p>
+        </div>
+    )
+}
--- crm-frontend/src/features/activities/pages/CompanyActivitiesPage.tsx
+++ crm-frontend/src/features/activities/pages/CompanyActivitiesPage.tsx
@@ -1,12 +1,44 @@
 import CreateButton from "../../../components/ui/buttons/CreateButton"
+import ActivityCard from "../components/ActivityCard"
 
 export default function CompanyActivitiesPage() {
 
     return(
-        <div className="w-full flex flex-col">
+        <div className="w-full flex flex-col pb-12">
             <div className="w-full flex justify-end mt-12">
                 <CreateButton title="Agregar Actividad" />
             </div>
+
+            <div className="w-full flex flex-col rounded-lg shadow-lg bg-[#171717] px-4 py-5 gap-4 mt-8">
+                <p className="px-3">Secuencia de actividades</p>
+
+                <div className="w-full rounded-md flex flex-col gap-3 bg-[#212121] p-3 ">
+                    <div className="flex gap-2 items-center">
+                        <p className="text-sm text-[#959595] ">Etapa: </p>
+                        <p className="text-sm ">Reconocimiento</p>
+                    </div>
+
+                    <div className="flex gap-8 items-center">
+                        <p className="text-sm text-[#959595]">12 Actividades registradas</p>
+                        <p className="text-sm text-[#959595]">Acercamiento inicial 12 abril 2026</p>
+                        <p className="text-sm text-[#959595]">1 Projecto Cerrado</p>
+                    </div>
+                </div>
+                <div className="w-full flex flex-col gap-8">
+                    <ActivityCard subject="Llamada con CISO (Fabiola Vega)" nextStep="Agendar sesion demostracion por zoom para el 15 de agosto a las 11 am" date="Mar 25, 2026 a las 10:30 am" result="Completado" type="call"  />
+                    <ActivityCard subject="Llamada con CISO (Fabiola Vega)" nextStep="Agendar sesion demostracion por zoom para el 15 de agosto a las 11 am" date="Mar 25, 2026 a las 10:30 am" result="Completado" type="email"  />
+                    <ActivityCard subject="Llamada con CISO (Fabiola Vega)" nextStep="Agendar sesion demostracion por zoom para el 15 de agosto a las 11 am" date="Mar 25, 2026 a las 10:30 am" result="Completado" type="meeting" />
+                    <ActivityCard subject="Llamada con CISO (Fabiola Vega)" nextStep="Agendar sesion demostracion por zoom para el 15 de agosto a las 11 am" date="Mar 25, 2026 a las 10:30 am" result="Completado" type="note"  />
+                    <ActivityCard subject="Llamada con CISO (Fabiola Vega)" nextStep="Agendar sesion demostracion por zoom para el 15 de agosto a las 11 am" date="Mar 25, 2026 a las 10:30 am" result="Completado" type="email"  />
+                    <ActivityCard subject="Llamada con CISO (Fabiola Vega)" nextStep="Agendar sesion demostracion por zoom para el 15 de agosto a las 11 am" date="Mar 25, 2026 a las 10:30 am" result="Completado" type="meeting" />
+                    <ActivityCard subject="Llamada con CISO (Fabiola Vega)" nextStep="Agendar sesion demostracion por zoom para el 15 de agosto a las 11 am" date="Mar 25, 2026 a las 10:30 am" result="Completado" type="call"  />
+                    <ActivityCard subject="Llamada con CISO (Fabiola Vega)" nextStep="Agendar sesion demostracion por zoom para el 15 de agosto a las 11 am" date="Mar 25, 2026 a las 10:30 am" result="Completado" type="email"  />
+                    <ActivityCard subject="Llamada con CISO (Fabiola Vega)" nextStep="Agendar sesion demostracion por zoom para el 15 de agosto a las 11 am" date="Mar 25, 2026 a las 10:30 am" result="Completado" type="note" isLast />
+                </div>
+
+
+            </div>
+
         </div>
     )
 }
